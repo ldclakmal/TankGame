f5b77eb3ec96b8cfd3fbcdc1ebbd5f8be1a479b7
Chanaka <ldclakmal@gmail.com>
2015-10-21 08:30:50 +0530

    GUI Form created
    JOIN# can be send via GUI and reply string can be received to
    messagebox. To be optimize using threads...

---
+++ ClientApplication/ClientApplication/GUI.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ClientApplication
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(464, 73);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "SEND";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 73);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.Size = new System.Drawing.Size(435, 23);
+            this.metroTextBox1.TabIndex = 1;
+            this.metroTextBox1.UseSelectable = true;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(23, 114);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Multiline = true;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.Size = new System.Drawing.Size(516, 163);
+            this.metroTextBox2.TabIndex = 2;
+            this.metroTextBox2.UseSelectable = true;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 300);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "GUI";
+            this.Text = "Tank Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+    }
+}
---
+++ ClientApplication/ClientApplication/GUI.cs
@@ -0,0 +1,109 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientApplication
+{
+    public partial class GUI : MetroForm
+    {
+        private TcpClient client;
+        private TcpListener listener;
+
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            //Program myPro = new Program();
+            while (true)
+            {
+                String cmd = metroTextBox1.Text;
+                sendCmd("127.0.0.1", 6000, cmd);
+                startListening("127.0.0.1", 7000);
+            }
+        }
+
+        public void sendCmd(String ip, int port, String data)
+        {
+            try
+            {
+                this.client = new TcpClient();
+                this.client.Connect(ip, port);
+
+                if (this.client.Connected)
+                {
+                    //To write to the socket
+                    NetworkStream clientStream = client.GetStream();
+                    BinaryWriter writer = new BinaryWriter(clientStream);
+                    //Create objects for writing across stream
+                    writer = new BinaryWriter(clientStream);
+                    Byte[] tempStr = Encoding.ASCII.GetBytes(data);
+
+                    //writing to the port                
+                    writer.Write(tempStr);
+                    //Console.WriteLine("\nData: " + data + " is written to " + ip + " on " + port);
+                    writer.Close();
+                    clientStream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("\nError at myPro:sendCmd..... " + e.StackTrace);
+                MessageBox.Show("\nError at myPro:sendCmd..... " + e.StackTrace);
+            }
+        }
+
+        public void startListening(String ip, int port)
+        {
+            //The socket that is listened to 
+            Socket connection = null;
+            try
+            {
+                //Creating listening Socket
+                this.listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7000);
+                //Starts listening
+                this.listener.Start();
+                //Establish connection upon client request
+                while (true)
+                {
+                    //connection is connected socket
+                    connection = listener.AcceptSocket();
+                    if (connection.Connected)
+                    {
+                        //To read from socket create NetworkStream object associated with socket
+                        NetworkStream serverStream = new NetworkStream(connection);
+
+                        SocketAddress sockAdd = connection.RemoteEndPoint.Serialize();
+                        string s = connection.RemoteEndPoint.ToString();
+                        List<Byte> inputStr = new List<byte>();
+
+                        int asw = 0;
+                        while (asw != -1)
+                        {
+                            asw = serverStream.ReadByte();
+                            inputStr.Add((Byte)asw);
+                        }
+
+                        String reply = Encoding.UTF8.GetString(inputStr.ToArray());
+                        serverStream.Close();
+                        string serverIp = s.Substring(0, s.IndexOf(":"));
+                        //Console.WriteLine(serverIp + ": " + reply);
+                        metroTextBox2.AppendText(serverIp + ": " + reply + "\n");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("\nError at myPro:startListening()..... " + e.StackTrace);
+                MessageBox.Show("\nError at myPro:startListening()..... " + e.StackTrace);
+            }
+        }
+    }
+}
